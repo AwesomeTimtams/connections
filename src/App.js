907ff61019aaa7338db35fc913386aae9f343273
AwesomeTimtams <48585541+AwesomeTimtams@users.noreply.github.com>
2025-07-23 10:32:49 +1000

    setting up react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Connections from './Connections.js';
+
+function App() {
+  return (
+    <Connections></Connections>
+  );
+}
+
+export default App;
